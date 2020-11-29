6faa0b263ae8273784ba70731f9a3f485659e7a0
Zaw Tun <zawtun@Zaws-iMac.local>
2020-11-29 17:06:04 -0500

    upadate cart count increse and decrese

--- src/Redux/cart/cart.reducer.js
+++ src/Redux/cart/cart.reducer.js
@@ -1,6 +1,6 @@
 
 import CartActionTypes from './cart.types';
-import {addItemToCart} from './cart.utils'
+import {addItemToCart, removeItemFromCart} from './cart.utils'
 
 
 const INITIAL_STATE = {
@@ -20,6 +20,22 @@ const cartReducer = (state = INITIAL_STATE, action) => {
                 ...state,
                 cartItems:addItemToCart(state.cartItems, action.payload)
             };
+        
+        case CartActionTypes.REMOVE_ITEM:
+            return{
+                ...state,
+                cartItems:removeItemFromCart(state.cartItems, action.payload)
+            };
+        
+
+        case CartActionTypes.CLEAR_ITEM_FORM_CART :
+            return{
+                ...state,
+                cartItems: state.cartItems.filter(
+                cartItem => cartItem.id !== action.payload.id
+                     )
+                };
+    
         default:
             return state;
     }
